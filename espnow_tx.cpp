00a9c33741be42b530da40c65a4e8cbca13c4b09
dima-dimka04 <57627727+dima-dimka04@users.noreply.github.com>
2025-03-28 00:05:42 +0300

    Create espnow_tx.cpp

---
+++ espnow_tx.cpp
@@ -0,0 +1,76 @@
+#include <Arduino.h>
+#ifdef ESP8266
+#include <ESP8266WiFi.h>
+#elif ESP32
+#include <WiFi.h>
+#endif
+#include "ESPNowW.h"
+#include <esp_wifi.h>
+
+uint8_t receiver_mac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x33};
+uint8_t broadcastAddress[] = {0xcc, 0x7b, 0x5c, 0x35, 0xc2, 0xb0};
+
+typedef struct struct_message {
+  char a[32];
+  int b;
+  float c;
+  bool d;
+} struct_message;
+struct_message myData;
+esp_now_peer_info_t peerInfo;
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+// получить MAC-адрес
+void readMacAddress(){
+  uint8_t baseMac[6];
+  esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
+  if (ret == ESP_OK) {
+    Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
+                  baseMac[0], baseMac[1], baseMac[2],
+                  baseMac[3], baseMac[4], baseMac[5]);
+  } else {
+    Serial.println("Failed to read MAC address");
+  }
+}
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("ESPNow sender Demo");
+#ifdef ESP8266
+    WiFi.mode(WIFI_STA); // MUST NOT BE WIFI_MODE_NULL
+#elif ESP32
+    WiFi.mode(WIFI_MODE_STA);
+    readMacAddress();
+#endif
+  WiFi.disconnect();
+  ESPNow.init();
+  esp_now_register_send_cb(OnDataSent);
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;   
+  peerInfo.encrypt = false;
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+}
+
+void loop() {
+  strcpy(myData.a, "THIS IS A CHAR");
+  myData.b = random(1,20);
+  myData.c = 1.2;
+  myData.d = false;
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  delay(2000);
+}
