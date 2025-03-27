32041e5067c1d4e3037f9004a0cb1e298d18c13a
dima-dimka04 <57627727+dima-dimka04@users.noreply.github.com>
2025-03-28 00:22:47 +0300

    Create tudasuda_espnow.cpp

---
+++ tudasuda_espnow.cpp
@@ -0,0 +1,250 @@
+#include <Arduino.h>
+#ifdef ESP8266
+#include <ESP8266WiFi.h>
+#elif ESP32
+#include <WiFi.h>
+#endif
+#include "ESPNowW.h"
+#include <esp_wifi.h>
+
+uint8_t broadcastAddress[] = {0xcc, 0x7b, 0x5c, 0x35, 0xc2, 0xb0}; // first board mac cc:7b:5c:35:c2:b0
+
+float temperature;
+float humidity;
+float pressure;
+
+// Define variables to store incoming readings
+float incomingTemp;
+float incomingHum;
+float incomingPres;
+
+// Variable to store if sending data was successful
+String success;
+
+//Structure example to send data
+//Must match the receiver structure
+typedef struct struct_message {
+    float temp;
+    float hum;
+    float pres;
+} struct_message;
+
+// Create a struct_message called BME280Readings to hold sensor readings
+struct_message BME280Readings;
+
+// Create a struct_message to hold incoming sensor readings
+struct_message incomingReadings;
+esp_now_peer_info_t peerInfo;
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  incomingTemp = incomingReadings.temp;
+  incomingHum = incomingReadings.hum;
+  incomingPres = incomingReadings.pres;
+}
+
+// получить MAC-адрес
+void readMacAddress(){
+  uint8_t baseMac[6];
+  esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
+  if (ret == ESP_OK) {
+    Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
+                  baseMac[0], baseMac[1], baseMac[2],
+                  baseMac[3], baseMac[4], baseMac[5]);
+  } else {
+    Serial.println("Failed to read MAC address");
+  }
+}
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("ESPNow sender Demo");
+#ifdef ESP8266
+    WiFi.mode(WIFI_STA); // MUST NOT BE WIFI_MODE_NULL
+#elif ESP32
+    WiFi.mode(WIFI_MODE_STA);
+    readMacAddress();
+#endif
+  WiFi.disconnect();
+  ESPNow.init();
+  esp_now_register_send_cb(OnDataSent);
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;   
+  peerInfo.encrypt = false;
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+}
+
+void getReadings(){
+  temperature = random(1,20);
+  humidity = random(70, 100);
+  pressure = random(720, 800);
+}
+
+void loop() {
+  getReadings();
+ 
+  // Set values to send
+  BME280Readings.temp = temperature;
+  BME280Readings.hum = humidity;
+  BME280Readings.pres = pressure;
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &BME280Readings, sizeof(BME280Readings));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+
+  Serial.println("INCOMING READINGS");
+  Serial.print("Temperature: ");
+  Serial.print(incomingReadings.temp);
+  Serial.println(" ºC");
+  Serial.print("Humidity: ");
+  Serial.print(incomingReadings.hum);
+  Serial.println(" %");
+  Serial.print("Pressure: ");
+  Serial.print(incomingReadings.pres);
+  Serial.println(" hPa");
+  Serial.println();
+  delay(2000);
+}
+
+/*-----------------------------------------------------------*/
+
+#include <Arduino.h>
+#ifdef ESP8266
+#include <ESP8266WiFi.h>
+#elif ESP32
+#include <WiFi.h>
+#endif
+#include "ESPNowW.h"
+#include <esp_wifi.h>
+
+uint8_t broadcastAddress[] = {0xcc, 0x7b, 0x5c, 0x35, 0xba, 0xcc}; // second board mac cc:7b:5c:35:ba:cc
+
+
+float temperature;
+float humidity;
+float pressure;
+
+// Define variables to store incoming readings
+float incomingTemp;
+float incomingHum;
+float incomingPres;
+
+// Variable to store if sending data was successful
+String success;
+
+//Structure example to send data
+//Must match the receiver structure
+typedef struct struct_message {
+    float temp;
+    float hum;
+    float pres;
+} struct_message;
+
+// Create a struct_message called BME280Readings to hold sensor readings
+struct_message BME280Readings;
+
+// Create a struct_message to hold incoming sensor readings
+struct_message incomingReadings;
+esp_now_peer_info_t peerInfo;
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  incomingTemp = incomingReadings.temp;
+  incomingHum = incomingReadings.hum;
+  incomingPres = incomingReadings.pres;
+}
+
+// получить MAC-адрес
+void readMacAddress(){
+  uint8_t baseMac[6];
+  esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
+  if (ret == ESP_OK) {
+    Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
+                  baseMac[0], baseMac[1], baseMac[2],
+                  baseMac[3], baseMac[4], baseMac[5]);
+  } else {
+    Serial.println("Failed to read MAC address");
+  }
+}
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("ESPNow sender Demo");
+#ifdef ESP8266
+    WiFi.mode(WIFI_STA); // MUST NOT BE WIFI_MODE_NULL
+#elif ESP32
+    WiFi.mode(WIFI_MODE_STA);
+    readMacAddress();
+#endif
+  WiFi.disconnect();
+  ESPNow.init();
+  esp_now_register_send_cb(OnDataSent);
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;   
+  peerInfo.encrypt = false;
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+}
+
+void getReadings(){
+  temperature = random(1,20);
+  humidity = random(70, 100);
+  pressure = random(720, 800);
+}
+
+void loop() {
+  getReadings();
+ 
+  // Set values to send
+  BME280Readings.temp = temperature;
+  BME280Readings.hum = humidity;
+  BME280Readings.pres = pressure;
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &BME280Readings, sizeof(BME280Readings));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+
+  Serial.println("INCOMING READINGS");
+  Serial.print("Temperature: ");
+  Serial.print(incomingReadings.temp);
+  Serial.println(" ºC");
+  Serial.print("Humidity: ");
+  Serial.print(incomingReadings.hum);
+  Serial.println(" %");
+  Serial.print("Pressure: ");
+  Serial.print(incomingReadings.pres);
+  Serial.println(" hPa");
+  Serial.println();
+  delay(2000);
+}
